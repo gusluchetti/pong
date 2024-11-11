ae055052f269ba51f74673bd2c3391672fd3238e
Gustavo Luchetti <gustavo@luchetti.dev>
2024-11-11 12:51:47 -0300

    feat: init window
    trying to use sdl and opengl

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
+#include <GL/gl.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_error.h>
-#include <cstdio>
-#include <iostream>
 
-#include <SDL2_image/SDL_image.h>
+#include <cstdio>
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 450;
@@ -12,47 +11,55 @@ SDL_Window *gWindow = NULL;
 SDL_Surface *gScreenSurface = NULL;
 SDL_Surface *gHelloWorld = NULL;
 
-// init sdl and opens window
-bool init() {
-  bool success = true;
+int main(int, char **) {
+  // Initialize SDL
+  SDL_Init(SDL_INIT_VIDEO);
 
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    success = false;
-  } else {
-    gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
-                               SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                               SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (gWindow == NULL) {
-      printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-      success = false;
-    } else {
-      gScreenSurface = SDL_GetWindowSurface(gWindow);
+  // Create a window and an OpenGL context
+  SDL_Window *window =
+      SDL_CreateWindow("pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                       SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
+  SDL_GLContext context = SDL_GL_CreateContext(window);
+
+  // Set up the OpenGL viewport
+  glViewport(0, 0, 800, 600);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(-1.0, 1.0, -1.0, 1.0, 0.0, 1.0);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+
+  // Game loop
+  bool running = true;
+  while (running) {
+    // Handle events
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        running = false;
+      }
     }
+
+    // Clear the screen
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Draw a white square
+    glColor3f(1.0, 1.0, 1.0);
+    glBegin(GL_QUADS);
+    glVertex2f(-0.1, -0.1);
+    glVertex2f(-0.1, 0.1);
+    glVertex2f(0.1, 0.1);
+    glVertex2f(0.1, -0.1);
+    glEnd();
+
+    // Update the display
+    SDL_GL_SwapWindow(window);
   }
 
-  return success;
-};
+  // Clean up
+  SDL_GL_DeleteContext(context);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 
-// loads media
-bool loadMedia() {
-  bool success = true;
-
-  gHelloWorld =
-      SDL_LoadBMP("02_getting_an_image_on_the_screen/hello_world.bmp");
-  if (gHelloWorld == NULL) {
-    printf("Unable to load image %s! SDL Error: %s\n",
-           "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError());
-    success = false;
-  }
-
-  return success;
-};
-
-// frees media and shuts down
-bool close();
-
-int main(int argc, char *argv[]) {
-  std::cout << "Hello World!" << std::endl;
   return 0;
 }
