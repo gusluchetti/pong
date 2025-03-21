0495cd9ae9377d433d6843f68e43e8600478518a
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2025-03-21 16:23:41 -0300

    feat: better defaults, and bound players inside window

--- src/pong.cpp
+++ src/pong.cpp
@@ -6,19 +6,21 @@
 #include <string>
 
 struct Ball {
-  float radius;
   Vector2 pos;
   Vector2 movementVector;
+  float radius = 5.5f;
 };
 
 struct Player {
   Rectangle rec;
-  int score;
+  int score = 0;
+  bool hitTop = false;
+  bool hitBottom = false;
 };
 
 std::random_device rd;
 std::mt19937 gen(rd());
-std::uniform_real_distribution<> dis(4.0, 7.0);
+std::uniform_real_distribution<> dis(5.0, 7.0);
 float randomX = dis(gen);
 float randomY = dis(gen);
 
@@ -31,8 +33,8 @@ Player p1, p2;
 std::string scoreUI;
 
 void DrawObjects() {
-  std::cout << p1.rec.x << " " << p1.rec.y;
-  std::cout << p2.rec.x << " " << p2.rec.y;
+  std::cout << "\np1 (left) " << p1.rec.x << " " << p1.rec.y;
+  std::cout << " | p2 (right) " << p2.rec.x << " " << p2.rec.y;
   ClearBackground(BLACK);
   DrawText(scoreUI.c_str(), w_Width / 2, 20, 30, WHITE);
   DrawCircle(ball.pos.x, ball.pos.y, ball.radius, RED);
@@ -40,18 +42,44 @@ void DrawObjects() {
   DrawRectangleRec(p2.rec, RED);
 }
 
+void BlockPlayers(Player *pList[2]) {
+  for (size_t i = 0; i <= 1; ++i) {
+    if (pList[i] != nullptr) {
+      Player *p = pList[i];
+      bool hitTop = CheckCollisionPointLine({p->rec.x, p->rec.y}, {0.0f, 0.0f},
+                                            {w_Width, 0.0f}, 1);
+      if (hitTop) {
+        p->hitTop = true;
+      } else {
+        p->hitTop = false;
+      }
+      bool hitBottom =
+          CheckCollisionPointLine({(p->rec.x), (p->rec.y + p->rec.height)},
+                                  {0.0f, w_Height}, {w_Width, w_Height}, 1);
+      if (hitBottom) {
+        p->hitBottom = true;
+      } else {
+        p->hitBottom = false;
+      }
+    }
+  }
+}
+
 void HandleEvents() {
-  if (IsKeyDown(KEY_W)) {
+  Player *playerList[2] = {&p1, &p2};
+  BlockPlayers(playerList);
+
+  if (IsKeyDown(KEY_W) && p1.hitTop == false) {
     p1.rec.y -= moveSpeed;
   }
-  if (IsKeyDown(KEY_UP)) {
-    p2.rec.y -= moveSpeed;
-  }
-
-  if (IsKeyDown(KEY_S)) {
+  if (IsKeyDown(KEY_S) && p1.hitBottom == false) {
     p1.rec.y += moveSpeed;
   }
-  if (IsKeyDown(KEY_DOWN)) {
+
+  if (IsKeyDown(KEY_UP) && p2.hitTop == false) {
+    p2.rec.y -= moveSpeed;
+  }
+  if (IsKeyDown(KEY_DOWN) && p2.hitBottom == false) {
     p2.rec.y += moveSpeed;
   }
 
@@ -82,11 +110,9 @@ void HandlePhysics() {
     std::cout << "\nhit player! " << ball.pos.x << " " << ball.pos.y;
   }
 
-  bool insideWindow = CheckCollisionCircleRec(ball.pos, ball.radius,
-                                              {0.0f, 0.0f, w_Width, w_Height});
-  std::cout << "\ninside! " << ball.pos.x << " " << ball.pos.y;
-  if (!insideWindow) {
-    std::cout << "\noutside! " << ball.pos.x << " " << ball.pos.y;
+  bool isBallInsideWindow = CheckCollisionCircleRec(
+      ball.pos, ball.radius, {0.0f, 0.0f, w_Width, w_Height});
+  if (!isBallInsideWindow) {
     if (ball.pos.x <= 0) {
       p2.score += 1;
     }
@@ -104,28 +130,27 @@ void HandlePhysics() {
 
 int main() {
   SetTargetFPS(60);
-  SetWindowState(FLAG_WINDOW_RESIZABLE);
   InitWindow(w_Width, w_Height, "pong");
+  SetWindowState(FLAG_WINDOW_RESIZABLE);
   // SetWindowState(FLAG_WINDOW_MAXIMIZED);
 
   w_Width = GetScreenWidth();
   w_Height = GetScreenHeight();
-  std::cout << w_Width << " " << w_Height;
 
-  std::cout << randomX << " " << randomY;
-  ball = {6.0f, {(float)w_Width / 2, (float)w_Height / 2}, {randomX, randomY}};
-  p1 = {{0.0f, 0.0f, 12, 108}, 0};
+  ball = {{(float)w_Width / 2, (float)w_Height / 2}, {randomX, randomY}};
+  p1 = {{0.0f, 0.0f, 12, 108}};
   p2 = {{w_Width - p1.rec.width, w_Height - p1.rec.height, p1.rec.width,
-         p1.rec.height},
-        p1.score};
+         p1.rec.height}};
   scoreUI = std::to_string(p1.score) + " - " + std::to_string(p2.score);
 
   std::cout << "\nstart!\n";
   while (!WindowShouldClose()) {
     BeginDrawing();
-    DrawObjects();
-    HandleEvents();
-    HandlePhysics();
+    {
+      DrawObjects();
+      HandleEvents();
+      HandlePhysics();
+    }
     EndDrawing();
   }
   std::cout << "\n!finish\n";
